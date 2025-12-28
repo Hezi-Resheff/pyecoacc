51fd1c7f145a69b25fbe3d24e2078fd7a7ed94fb
zeekresheff <yehezkel.resheff@mail.huji.ac.il>
2025-12-28 10:10:08 +0200

    CNN is not flex

--- pyecoacc/models/deep/cnn.py
+++ pyecoacc/models/deep/cnn.py
@@ -12,61 +12,60 @@ from sklearn.pipeline import Pipeline
 
 
 class BehaviorCNNClassifier(nn.Module):
-    def __init__(self, num_classes, sequence_length):
+    def __init__(self, num_classes, sequence_length, conv_filters=[32, 64, 128, 256], 
+                 fc_layers=[256, 512], kernel_size=5):
         super().__init__()
 
-        # Input shape: (batch_size, 3 ACC axes XYZ, sequence_length = sampling rate [Hz] * duration [s])
+        self.conv_blocks = nn.ModuleList()
+        in_channels = 3  # XYZ 
+        
+        # 1. Dynamically build Convolutional Blocks
+        for out_channels in conv_filters:
+            block = nn.Sequential(
+                nn.Conv1d(in_channels, out_channels, kernel_size=kernel_size, stride=1, padding=kernel_size//2),
+                nn.BatchNorm1d(out_channels),
+                nn.ReLU(),
+                nn.MaxPool1d(kernel_size=2, stride=2),
+                nn.Dropout(0.4)
+            )
+            self.conv_blocks.append(block)
+            in_channels = out_channels
 
-        # First convolutional layer
-        self.conv1 = nn.Conv1d(in_channels=3, out_channels=32, kernel_size=5, stride=1, padding=2)
-        self.bn1 = nn.BatchNorm1d(32)
-        self.pool1 = nn.MaxPool1d(kernel_size=2, stride=2)
+        self.flattened_size = self._get_flatten_size(sequence_length)
 
-        # Second convolutional layer
-        self.conv2 = nn.Conv1d(in_channels=32, out_channels=64, kernel_size=5, stride=1, padding=2)
-        self.bn2 = nn.BatchNorm1d(64)
-        self.pool2 = nn.MaxPool1d(kernel_size=2, stride=2)
+        # 2. Dynamically build Fully Connected Layers
+        self.fc_blocks = nn.ModuleList()
+        curr_features = self.flattened_size
+        
+        for hidden_units in fc_layers:
+            self.fc_blocks.append(nn.Linear(curr_features, hidden_units))
+            self.fc_blocks.append(nn.ReLU())
+            self.fc_blocks.append(nn.Dropout(0.4))
+            curr_features = hidden_units
 
-        # Third convolutional layer
-        self.conv3 = nn.Conv1d(in_channels=64, out_channels=128, kernel_size=5, stride=1, padding=2)
-        self.bn3 = nn.BatchNorm1d(128)
-        self.pool3 = nn.MaxPool1d(kernel_size=2, stride=2)
+        # Final Output Layer
+        self.output_layer = nn.Linear(curr_features, num_classes)
 
-        # 4th convolutional layer
-        self.conv4 = nn.Conv1d(in_channels=128, out_channels=256, kernel_size=5, stride=1, padding=2)
-        self.bn4 = nn.BatchNorm1d(256)
-        self.pool4 = nn.MaxPool1d(kernel_size=2, stride=2)
-
-        # Fully connected layers
-        self.fc1 = nn.Linear(256 * (sequence_length // 16), 512)
-        self.fc2 = nn.Linear(512, num_classes)
-
-        # to be used everywhere
-        self.dropout = nn.Dropout(0.4)
+    def _get_flatten_size(self, sequence_length):
+        """Passes a dummy tensor through conv layers to find the flat shape."""
+        with torch.no_grad():
+            # (Batch, Channels, Length)
+            dummy_input = torch.zeros(1, 3, sequence_length)
+            for block in self.conv_blocks:
+                dummy_input = block(dummy_input)
+            return dummy_input.numel()
 
     def forward(self, x):
-        # Convolutional layers with norm ReLU and pooling
-        x = self.pool1(F.relu(self.bn1(self.conv1(x))))
-        x = self.dropout(x)
-        x = self.pool2(F.relu(self.bn2(self.conv2(x))))
-        x = self.dropout(x)
-        x = self.pool3(F.relu(self.bn3(self.conv3(x))))
-        x = self.dropout(x)
-        x = self.pool4(F.relu(self.bn4(self.conv4(x))))
+        for block in self.conv_blocks:
+            x = block(x)
 
-        # Flatten the output for the fully connected layer
         x = x.view(x.size(0), -1)
 
-        # Fully connected layers
-        x = F.relu(self.fc1(x))
-        x = self.dropout(x)
-        x = self.fc2(x)
-        return x
+        for fc_layer in self.fc_blocks:
+            x = fc_layer(x)
 
+        return self.output_layer(x)
 
-def arrange_acc_for_cnn(X):
-    sequence_len = X.shape[1] // 3
-    return np.transpose(X.reshape(-1, sequence_len, 3), (0, 2, 1)).astype(np.float32)
 
 
 class CNNInputReshaper(BaseEstimator, TransformerMixin):
@@ -79,19 +78,31 @@ class CNNInputReshaper(BaseEstimator, TransformerMixin):
     def fit(self, X, y=None):
         return self
 
+    @staticmethod
+    def arrange_acc_for_cnn(X):
+        sequence_len = X.shape[1] // 3
+        return np.transpose(X.reshape(-1, sequence_len, 3), (0, 2, 1)).astype(np.float32)
+
     def transform(self, X):
-        return arrange_acc_for_cnn(X)
+        return self.arrange_acc_for_cnn(X)
 
 
-def make_cnn_model(input_dim, num_behav, max_epoch=200, lr=.001, l2=5e-4, verbose=1, validation=.2, patience=50):
+def make_cnn_model(input_dim, num_behav, 
+                   conv_filters=[32, 64, 128, 256], 
+                   fc_layers=[256, 512], 
+                   kernel_size=5,
+                   max_epoch=200, lr=.001, l2=5e-4, verbose=1, validation=.2, patience=50, allow_cuda=True):
+    
     net = NeuralNetClassifier(
         module=BehaviorCNNClassifier,
-        criterion=nn.CrossEntropyLoss,
-        optimizer__weight_decay=l2,
-
+        module__conv_filters=conv_filters,
+        module__fc_layers=fc_layers,
+        module__kernel_size=kernel_size,  
         module__num_classes=num_behav,
         module__sequence_length=input_dim,
-
+        criterion=nn.CrossEntropyLoss,
+        optimizer__weight_decay=l2,
+        
         max_epochs=max_epoch,
         lr=lr,
         optimizer=torch.optim.Adam,
@@ -103,7 +114,7 @@ def make_cnn_model(input_dim, num_behav, max_epoch=200, lr=.001, l2=5e-4, verbos
         ],
 
         verbose=verbose,
-        device='cpu'
+        device='cuda' if torch.cuda.is_available() and allow_cuda else 'cpu'
     )
 
     return Pipeline([
